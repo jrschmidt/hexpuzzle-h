3e8731d36e344653456d1b361fab6156c532a16b
JR Schmidt <jrschmidt56@gmail.com>
2013-09-27 19:17:30 -0700

    generate data string from actual data

--- puzzle_server.rb
+++ puzzle_server.rb
@@ -21,27 +21,66 @@ end
 
 class PuzzlePattern
 
+  TOP_BOTTOM_PIECES = 6
+  MID_ROW_PIECES = 4
+  VERT_LANE_WIDTH = 4
+
+  ID_START = :a
+  
+
   attr :pattern_string
 
-#  def initialize
-#    @pattern_string = ""
-#    @grid = new PuzzleGrid
-#  end
+  def initialize
+    @grid = []
+#    shuffle_piece_sizes
+#    fill_top_row
+#    fill_bottom_row
+#    fill_center_lane
+
+    test = []
+    test << "aaaaabbbccccdddddeeeffff"
+    test << "aaabbbbbccccdddeeeeeffff"
+    test << "aaaabbbbccccddddeeeeffff"
+    test << "aaaggbghhhhcdiddiiiejjjf"
+    test << "ggggggghhhhhiiiiiiijjjjj"
+    test << "gggglglghhhhiiiioiojjjjj"
+    test << "kgkklllhmhmmninnooojpppj"
+    test << "kkkklllllmmmmnnnoooopppp"
+    test << "kkklklllmmmmnnnnooopoppp"
+    test << "xkxkxlxmxmxnxnxnxoxoxpxp"
+    0.upto(9) do |rr|
+      str = test[rr]
+      row = []
+      0.upto(str.size-1) {|cc| row << str[cc].to_sym}
+      @grid << row
+    end 
+  end
+
+
+  def fill_top_row
+
+  end
+
+
+  def fill_bottom_row
+
+  end
+
+
+  def fill_center_lane
+
+  end
+
+
+  def shuffle_piece_sizes
+
+  end
 
 
   def to_dom_string
-    test_string = ""
-    test_string << "aaaaabbbccccdddddeeeffff"
-    test_string << "aaabbbbbccccdddeeeeeffff"
-    test_string << "aaaabbbbccccddddeeeeffff"
-    test_string << "aaaggbghhhhcdiddiiiejjjf"
-    test_string << "ggggggghhhhhiiiiiiijjjjj"
-    test_string << "gggglglghhhhiiiioiojjjjj"
-    test_string << "kgkklllhmhmmninnooojpppj"
-    test_string << "kkkklllllmmmmnnnoooopppp"
-    test_string << "kkklklllmmmmnnnnooopoppp"
-    test_string << "xkxkxlxmxmxnxnxnxoxoxpxp"
-    test_string
+    str = ""
+    @grid.each {|row| row.each {|hx| str << hx.to_s} }
+    str
   end
 
 end
@@ -52,3 +91,4 @@ def puzzle_string
   @puzzle = PuzzlePattern.new
   @puzzle.to_dom_string
 end
+
