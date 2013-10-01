9c3d586d048fd36e998d6b2d4c382ac4f1b24a9b
JR Schmidt <jrschmidt56@gmail.com>
2013-09-30 19:00:30 -0700

    augment top & bottom pieces

--- puzzle_server.rb
+++ puzzle_server.rb
@@ -23,22 +23,14 @@ end
 
 class PuzzlePattern
 
-  TOP_BOTTOM_PIECES = 6
-  MID_ROW_PIECES = 4
-  VERT_LANE_WIDTH = 4
   SYM = [:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k,:l,:m,:n,:o,:p]
-  SIZES_START = [13,13,13,13,13,13,15,15,15,15,13,13,13,13,13,13]
-  SEVENTEENS = [ [6,7], [6,8], [6,9], [7,8], [7,9], [8,9] ]
-  TOP_BOTTOM = [0,1,2,3,4,5,10,11,12,13,14,15]
-  CENTER_LANE = [6,7,8,9]
-  ID_START = :a
-  
+  SIZES_START = [13,13,13,13,13,13,16,16,16,16,13,13,13,13,13,13]
 
   attr :pattern_string
 
   def initialize
-    @piece_sizes = shuffle_piece_sizes
     @grid = get_init_grid
+    @piece_sizes = shuffle_piece_sizes
     build_piece_patterns
   end
 
@@ -46,9 +38,9 @@ class PuzzlePattern
   def build_piece_patterns
     init_all_rows
 
-#    fill_top_row
-#    fill_bottom_row
-#    fill_center_lane
+    complete_top_row
+    complete_bottom_row
+
   end
 
 
@@ -75,49 +67,46 @@ class PuzzlePattern
   end
 
 
-#  def fill_top_row
-#    top = @piece_sizes[0..5]
-#    0.upto(5) {|n| build_top_piece(n,SYM[n],top[n]) }
-#  end
+  def complete_top_row
+    top = @piece_sizes[0..5]
+    0.upto(5) {|pc| augment_top_piece(pc,SYM[pc],top[pc]) }
+  end
 
 
-#  def build_top_piece(n,sym,size)
-#    [0,1,2,3].each {|aa| [0,1,2].each {|bb| set_hex(4*n+aa,bb,sym)} }
-#    if size == 13
-#      j = rand(2)
-#      set_hex(4*n+1+2*j,3,sym)
-#    end
-#    if size == 15
-#      k = rand(4)
-#      4.times {|i| set_hex(4*n+i,3,sym) unless i == k}
-#    end
-#  end
+  def augment_top_piece(pc,sym,size)
+    if size == 13
+      i = rand(2)*2
+      set_hex(4*pc+1+i,3,sym)
+    end
+    if size == 15
+      k = rand(2)*2
+      4.times {|i| set_hex(4*pc+i,3,sym) unless i == k}
+    end
+  end
 
 
-#  def fill_bottom_row
-#    bottom = @piece_sizes[10..15]
-#    10.upto(15) {|n| build_bottom_piece(n,SYM[n],bottom[n-10]) }
-#  end
+  def complete_bottom_row
+    bottom = @piece_sizes[10..15]
+    10.upto(15) {|pc| augment_bottom_piece(pc,SYM[pc],bottom[pc-10]) }
+  end
 
 
-#  def build_bottom_piece(n,sym,size)
-#    [1,3].each {|aa| set_hex(4*(n-10)+aa,9,sym)}
-#    [0,1,2,3].each {|aa| [8,7,6].each {|bb| set_hex(4*(n-10)+aa,bb,sym)} }
-#    if size == 13
-#      j = rand(2)
-#      set_hex(4*(n-10)+1+2*j,6,:x)
-#    end
-#    if size == 15
-#      k = rand(2)
-#      set_hex(4*(n-10)+2*k,5,sym)
-#    end
-#  end
+  # FIXME FIX THIS METHOD
+  def augment_bottom_piece(pc,sym,size)
+    if size == 13
+      i = 1+rand(2)*2
+      set_hex(4*(pc-10)+i,6,sym)
+    end
+    if size == 15
+      [1,3].each {|i| set_hex(4*(pc-10)+i,6,sym) }
+      i = rand(2)*2
+      set_hex(4*(pc-10)+i,5,sym)
+    end
+  end
 
 
   def shuffle_piece_sizes
     sizes = SIZES_START
-
-    # Top & bottom rows
     fifteens = 0
     until fifteens == 4 do
       r6 = rand(6)
@@ -127,14 +116,6 @@ class PuzzlePattern
         fifteens = fifteens+1      
       end
     end    
-
-    # TODO With the new algorithm ('center first'), we no longer need to pick
-    # random values for the center lane pieces.
-
-    # Center lane
-    r6 = rand(6)
-    p17 = SEVENTEENS[r6]
-    p17.each {|i| sizes[i] = 17}
     sizes
   end
 
